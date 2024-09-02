54f0bf1418fce70b95ba72c3fbccee90cb2f8e49
Shiv <69760883+shivshankar9@users.noreply.github.com>
2024-09-02 16:24:08 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,  // No longer needed in mongoose v6+
-      useUnifiedTopology: true, // No longer needed in mongoose v6+
+      useNewUrlParser: true, 
+      useUnifiedTopology: true, 
     });
     console.log('MongoDB Connected...');
   } catch (error) {
